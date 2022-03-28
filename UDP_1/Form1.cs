a013cde640993fa4c370ea1543f85c540446ea38
user <user@DESKTOP-6JBEF5K>
2022-03-28 11:16:19 +0800

    嘗試宣告預設IP失敗

--- UDP_1/Form1.cs
+++ UDP_1/Form1.cs
@@ -25,14 +25,22 @@ namespace UDP_1
         private void listen()
         {
             int port = int.Parse(textBox_port.Text);
-            U = new UdpClient(port);
-            IPEndPoint EP = new IPEndPoint(IPAddress.Parse("127.0.0.1"), port);
-
-            while (true)
-            {
-                byte[] B = U.Receive(ref EP);
-                textBox_ReceiveText.Text = Encoding.Default.GetString(B);
+            if (textBox_port.Text!=null)
+            {                
+                U = new UdpClient(port);
+                IPEndPoint EP = new IPEndPoint(IPAddress.Parse("127.0.0.1"), port);
+                while (true)
+                {
+                    byte[] B = U.Receive(ref EP);
+                    textBox_ReceiveText.Text = Encoding.Default.GetString(B);
+                }
             }
+            else 
+            {
+                U = new UdpClient();
+                IPEndPoint EP = new IPEndPoint(IPAddress.Parse("127.0.0.1"), port);
+            } 
+
         }
         private void Form1_Load(object sender, EventArgs e)
         {
@@ -54,9 +62,11 @@ namespace UDP_1
 
         private void Form1_FormClosing(object sender, FormClosingEventArgs e)
         {
+            Th = new Thread(listen);
+            Th.Start();
+            
             try
-            {
-                
+            {                
                 Th.Abort();
                 U.Close();
             }
@@ -74,6 +84,11 @@ namespace UDP_1
             UdpClient S = new UdpClient();
             S.Send(B,B.Length,IP,port);
             S.Close();
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+           
         }
     }
 }
